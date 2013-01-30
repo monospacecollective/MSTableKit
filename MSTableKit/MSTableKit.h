2a032582395707ee77a40f9748abe12e5b1eb77b
Eric Horacek <horacek.eric@gmail.com>
2013-01-30 15:59:32 -0700

    Adds a header file that combines all headers into just one import

---
+++ MSTableKit/MSTableKit.h
@@ -0,0 +1,10 @@
+//
+//  MSTableKit.h
+//  MSTableKit
+//
+//  Created by Eric Horacek on 1/18/13.
+//  Copyright (c) 2013 Monospace Ltd. All rights reserved.
+//
+
+#import "MSGroupedTableView.h"
+#import "MSPlainTableView.h"
